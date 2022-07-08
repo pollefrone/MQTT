588f0bee41ee870caa9bba2ee5e0908b8cb646c1
pollefrone <31295233+pollefrone@users.noreply.github.com>
2022-07-08 20:12:49 -0300

    Update mqtt.py

--- mqtt.py
+++ mqtt.py
@@ -1,5 +1,9 @@
 # Para instalar o paho-mqtt use o comando pip install paho-mqtt
+
 import paho.mqtt.client as mqtt
+#import datetime 
+
+nomes = ["b'Pollefra'","b'Pollefra2'"]
 
 # O retorno de chamada para quando o cliente recebe uma resposta CONNACK do servidor.
 def on_connect(client, userdata, flags, rc):
@@ -7,11 +11,16 @@ def on_connect(client, userdata, flags, rc):
     # O subscribe fica no on_connect pois, caso perca a conexão ele a renova
     # Lembrando que quando usado o #, você está falando que tudo que chegar após a barra do topico, será recebido
     client.subscribe("v3r_nome_senai")
-
 # Callback responável por receber uma mensagem publicada no tópico acima
 def on_message(client, userdata, msg):
-    print(msg.topic + " -  " + str(msg.payload))
-    #print(msg.payload)
+    #print(msg.topic + " -  " + str(msg.payload))
+    informacacao_esp32 = str(msg.payload)
+    print(informacacao_esp32)
+    for nome in nomes:
+        #print(nome)
+        if nome == informacacao_esp32:
+            print("Hora registrada")
+            # if informacacao_esp32 ==
 
 client = mqtt.Client()
 client.on_connect = on_connect
